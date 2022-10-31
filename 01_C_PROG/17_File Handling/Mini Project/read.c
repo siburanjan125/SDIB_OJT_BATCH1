dd199be8f6654f78e5845a6ac6ed7599daf60fa8
siburanjan125 <109842407+siburanjan125@users.noreply.github.com>
2022-10-31 17:21:59 +0530

    Add files via upload

---
+++ 01_C_PROG/17_File Handling/Mini Project/read.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void read(void);
+
+typedef struct 
+{
+    /* data */
+    char name[40],gender;
+    int age,roll;
+}student;
+
+void read()
+{
+    char ch;
+    student s;
+    FILE *fp;
+    fp = fopen("student1.dat","r");
+    printf("\nStudents Details\n");
+    while((fscanf(fp,"%d %[^\t] %d %c",&s.roll,s.name,&s.age,&s.gender))!=EOF)
+    {
+        printf("\n%4d %-20s %4d %1c",s.roll,s.name,s.age,s.gender);
+    }
+    printf("\n");
+    fclose(fp);
+}
---
+++ 01_C_PROG/17_File Handling/Mini Project/studata.h
@@ -0,0 +1,3 @@
+#include<stdio.h>
+void read(void);
+void write(void);
