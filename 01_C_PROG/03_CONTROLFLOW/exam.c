435d2e6df0152a39ed20f2ef83760a05ea2f4601
siburanjan125 <109842407+siburanjan125@users.noreply.github.com>
2022-10-31 16:21:24 +0530

    Delete 01_C_PROG/03_CONTROLFLOW directory

--- 01_C_PROG/03_CONTROLFLOW/exam.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    int z, x=5, y= -10, a=4, b=2;
-    z = x++ - --y*b/a;
-        printf("%d",z);
-}
--- 01_C_PROG/03_CONTROLFLOW/pr9.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    int b =15, c=5,d =8,e =8,a;
-    a = b>c ? c>d ? 12: d>e ? 13:14:15;    
-    printf("%d\n",a);
-    
-}
